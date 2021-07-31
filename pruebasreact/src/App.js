07664b7a10c81944d408d48ea7498e749b5ed75e
sorceris <spartan403@gmail.com>
2021-07-30 19:26:52 -0600

    Primer commit guia 2

---
+++ pruebasreact/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div className="App-content">
+    <p>Aquí haremos nuestro TO-DO list</p>
+    </div>
+    </div>
+  );
+}
+
+export default App;
